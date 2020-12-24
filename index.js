418b71b0c86fd7ce6eaa7856905937db4d2458d1
Alejandro Pacios <pacios1687@gmail.com>
2020-12-24 09:52:12 +0100

    ajustes

--- index.js
+++ index.js
@@ -18,12 +18,13 @@ app.use( express.json({ extended: true }));
 const port = process.env.PORT || 4000;
 
 // Importar rutas
+
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 // arrancar la app
-app.listen(port,  () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
